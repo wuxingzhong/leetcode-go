5eee7ae6ff25da79a04134cfaa3cd5b59f2535f6
wuxingzhong <330338212@qq.com>
2020-11-19 14:29:32 +0800

    add

--- 2.两数相加.go
+++ 2.两数相加.go
@@ -13,7 +13,37 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var (
+		head, tail *ListNode
+	)
+	num, carry := 0, 0
+	for l1 != nil || l2 != nil {
+		n1, n2 := 0, 0
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := n1 + n2 + carry
+		num, carry = sum%10, sum/10
 
+		if head == nil {
+			head = &ListNode{Val: num}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: num}
+			tail = tail.Next
+		}
+
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return head
 }
+
 // @lc code=end
 
